e9faedcb623a7ac1b4bf7f0c0ff4918aef899016
SedTheCursed <sed.robert@gmail.com>
2019-05-16 15:53:57 +0200

    exercice 6

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,9 @@
 //hello();
 //factorielle();
 //pyramide();
-//new Entrepreneurs(businesspeople).perform()
-new Library(books).perform()
+//new Entrepreneurs(businesspeople).perform();
+//new Library(books).perform();
+//"CCGUCGUUGCGCUACAGC" => "Proline-Sérine-Leucine-Arginine-Tyrosine-Sérine"
+console.log(new Genome("CCGUCGUUGCGCUACAGC").proteins);
+//"CCUCGCCGGUACUUCUCG" => "Proline-Arginine-Arginine-Tyrosine-Phénylalanine-Sérine"
+console.log(new Genome("CCUCGCCGGUACUUCUCG").proteins);
---
+++ scripts/script6.js
@@ -0,0 +1,61 @@
+class Genome {
+    constructor(arn) {
+        this.sequence = this.sequencedARN(arn);
+        this.proteins = this.ARNtoProteins();
+    }
+
+    sequencedARN(arn) {
+        let sequence = ""
+
+        for(let i=0, l = arn.length/3; i < l; i++) {
+            sequence += this.subSequence(arn, i)
+            sequence += (i !== l-1) ? "-" : "";
+        }
+        
+        return sequence
+    }
+
+    subSequence(arn, brick) {
+        return arn.substr(brick*3, 3)
+    }
+
+    ARNtoProteins() {
+        return this.sequence
+            .split("-")
+            .map (subsequence => {
+                switch(subsequence) {
+                    case "UCU":
+                    case "UCC":
+                    case "UCA":
+                    case "UCG":
+                    case "AGU":
+                    case "AGC":
+                        return "Sérine";
+                    case "CCU":
+                    case "CCC":
+                    case "CCA":
+                    case "CCG":
+                        return "Proline";
+                    case "UUA":
+                    case "UUG":
+                        return "Leucine";
+                    case "UUU":
+                    case "UUC":
+                        return "Phénylalanine";
+                    case "CGU":
+                    case "CGC":
+                    case "CGA":
+                    case "CGG":
+                    case "AGA":
+                    case "AGG":
+                        return "Arginine";
+                    case "UAU":
+                    case "UAC":
+                        return "Tyrosine";
+                    default :
+                        return ""
+                }
+            })
+            .join("-")
+    }
+}
